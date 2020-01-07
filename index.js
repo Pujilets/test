b4991a9cfdcce530a79dd8d6f8961af011027dd9
Pujilets <pujiantari@gmail.com>
2020-01-07 11:24:35 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    return res.send('Welcome to the beginning of nothingness')
+})
+
+
+app.listen(3300, () => {
+    console.log('Example app listening on port 3300')
+})
